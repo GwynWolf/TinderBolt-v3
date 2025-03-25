1b2af9625b8fc8cc51ac52121d766702127432c4
Vlad <chvs1312@gmail.com>
2025-03-25 09:10:27 +0200

    refactor(TinderBotApp.java): add DialogMode

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -16,6 +16,7 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = ""; //TODO: додай ім'я бота в лапках
     public static final String TELEGRAM_BOT_TOKEN = ""; //TODO: додай токен бота в лапках
     public static final String OPEN_AI_TOKEN = ""; //TODO: додай токен ChatGPT у лапках
+    public DialogMode dialogMode = DialogMode.MAIN;
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -27,13 +28,31 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
         switch (update.getMessage().getText()) {
             case("/start"):
             {
+                dialogMode = DialogMode.MAIN;
                 text = loadMessage("main");
                 sendPhotoMessage("main");
                 sendTextMessage(text);
                 return;
             }
+            case("/profile"):
+            {
+                dialogMode = DialogMode.PROFILE;
+            }
+            case("/opener"):
+            {
+                dialogMode = DialogMode.OPENER;
+            }
+            case("/message"):
+            {
+                dialogMode = DialogMode.MESSAGE;
+            }
+            case("/date"):
+            {
+                dialogMode = DialogMode.DATE;
+            }
             case("/gpt"):
             {
+                dialogMode = DialogMode.GPT;
                 text = loadMessage("gpt");
                 //currentMode = DialogMode.GPT;
                 sendPhotoMessage("gpt");
@@ -41,6 +60,8 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
                 return;
             }
         }
+
+
 
 
     }
